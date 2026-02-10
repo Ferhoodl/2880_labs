5d65491c6f64346f81017031ff6f9efca358200b
Mistry <mmistry@iastate.edu>
2026-02-10 13:56:43 -0600

    End of lab progress

--- Lab3/mission1.c
+++ Lab3/mission1.c
@@ -15,21 +15,26 @@
 #include "mission1.h"
 
 void main(){
+    lcd_init();
+    cyBot_uart_init();
+    lcd_printf("test 0!");
     getMessage();
 }
 
 void getMessage(){
-    char currentChar;
+    lcd_printf("test 1!");
+    int currentChar;
     char message[50];
     int charIndex = 0;
     currentChar = cyBot_getByte();
 
-    cyBot_uart_init();
-    lcd_init();
 
+    lcd_printf("test 2!");
     while (charIndex < 50 && currentChar != '\0' ){
         message[charIndex] = currentChar;
-        lcd_printf("%s", message); // displays input
+        lcd_printf("Message: %s", message); // displays input
+        currentChar = cyBot_getByte();
+        charIndex += 1;
     }
 
 }
