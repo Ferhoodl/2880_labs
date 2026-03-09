fd7fbf36acd5cf3a819212057aa4617014cb827b
Mistry <mmistry@iastate.edu>
2026-03-09 18:44:13 -0500

    updated uart-interrupt.c

--- Lab6/uart-interrupt.c
+++ Lab6/uart-interrupt.c
@@ -22,30 +22,31 @@ volatile int command_flag = 0; // flag to tell the main program a special comman
 void uart_interrupt_init(void){
 	//TODO
   //enable clock to GPIO port B
-  SYSCTL_RCGCGPIO_R |= ???
+  SYSCTL_RCGCGPIO_R |= 0x02;
 
   //enable clock to UART1
-  SYSCTL_RCGCUART_R |= ???
+  SYSCTL_RCGCUART_R |= 0x02;
 
   //wait for GPIOB and UART1 peripherals to be ready
-  while ((SYSCTL_PRGPIO_R & ???) == 0) {};
-  while ((SYSCTL_PRUART_R & ???) == 0) {};
+  while ((SYSCTL_PRGPIO_R & 0x02) == 0) {};
+  while ((SYSCTL_PRUART_R & 0x02) == 0) {};
 
   //enable digital functionality on port B pins
-  GPIO_PORTB_DEN_R |= ???
+  GPIO_PORTB_DEN_R |= 0x03;
 
   //enable alternate functions on port B pins
-  GPIO_PORTB_AFSEL_R |= ???
+  GPIO_PORTB_AFSEL_R |= 0x03;
 
   //enable UART1 Rx and Tx on port B pins
-  GPIO_PORTB_PCTL_R = ???
+  GPIO_PORTB_PCTL_R &= 0xFFFFFF00;       // Force 0's in the desired locations
+  GPIO_PORTB_PCTL_R |= 0x00000011;       // Force 1's in the desired locations
 
   //calculate baud rate
-  uint16_t iBRD = ???; //use equations
-  uint16_t fBRD = ???; //use equations
+  uint16_t iBRD = 8; //use equations (got values from Bai textbook page 662)
+  uint16_t fBRD = 44; //use equations (got values from Bai textbook page 662)
 
   //turn off UART1 while setting it up
-  UART1_CTL_R &= ???
+  UART1_CTL_R &= 0xFFFFFFFE; // got from Bai textbook page 662
 
   //set baud rate
   //note: to take effect, there must be a write to LCRH after these assignments
@@ -54,13 +55,13 @@ void uart_interrupt_init(void){
 
   //set frame, 8 data bits, 1 stop bit, no parity, no FIFO
   //note: this write to LCRH must be after the BRD assignments
-  UART1_LCRH_R = ???
+  UART1_LCRH_R = 0x60; // got from Bai textbook page 662
 
   //use system clock as source
   //note from the datasheet UARTCCC register description:
   //field is 0 (system clock) by default on reset
   //Good to be explicit in your code
-  UART1_CC_R = 0x0;
+  UART1_CC_R = 0x00;
 
   //////Enable interrupts
 
@@ -68,13 +69,13 @@ void uart_interrupt_init(void){
   UART1_ICR_R |= 0b00010000;
 
   //enable RX raw interrupts in interrupt mask register
-  UART1_IM_R |= ?????;
+  UART1_IM_R |= 0b00010000;
 
   //NVIC setup: set priority of UART1 interrupt to 1 in bits 21-23
   NVIC_PRI1_R = (NVIC_PRI1_R & 0xFF0FFFFF) | 0x00200000;
 
   //NVIC setup: enable interrupt for UART1, IRQ #6, set bit 6
-  NVIC_EN0_R |= ?????;
+  NVIC_EN0_R |= 0x40;
 
   //tell CPU to use ISR handler for UART1 (see interrupt.h file)
   //from system header file: #define INT_UART1 22
@@ -89,28 +90,31 @@ void uart_interrupt_init(void){
   //Good to be explicit in your code
   //Be careful to not clear RX and TX enable bits
   //(either preserve if already set or set them)
-  UART1_CTL_R = ???
+  UART1_CTL_R |= 0b1100000001; // UART1_CTL_R = ??? was the original line, but page 662 of Bai textbook uses what is not commented
 
 }
 
 void uart_sendChar(char data){
-	//TODO
+    while((UART1_FR_R & 0x20) != 0);
+        UART1_DR_R = data;
 }
 
-char uart_receive(void){
+/*char uart_receive(void){
 	//DO NOT USE this busy-wait function if using RX interrupt
-}
+}*/
 
 void uart_sendStr(const char *data){
-	//TODO for reference see lcd_puts from lcd.c file
-}
+    while (*data != '\0') {
+            uart_sendChar(*data);
+            data++;
+        }}
 
 // Interrupt handler for receive interrupts
 void UART1_Handler(void)
 {
     char byte_received;
     //check if handler called due to RX event
-    if (UART1_MIS_R & ?????)
+    if (UART1_MIS_R & 0x10)
     {
         //byte was received in the UART data register
         //clear the RX trigger flag (clear by writing 1 to ICR)
@@ -118,8 +122,8 @@ void UART1_Handler(void)
 
         //read the byte received from UART1_DR_R and echo it back to PuTTY
         //ignore the error bits in UART1_DR_R
-        byte_received = ?????;
-        uart_sendChar(?????);
+        byte_received = (char)(UART1_DR_R & 0xFF);
+        uart_sendChar(byte_received);
 
         //if byte received is a carriage return
         if (byte_received == '\r')
